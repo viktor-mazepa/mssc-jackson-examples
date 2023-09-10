516fbe14c3a6237a5a927e89a450849e723be5cc
viktor-mazepa <viktor.mazepa@gmail.com>
2023-09-10 15:08:30 +0300

    Add Kebab Naming Strategy test and small test refactoring

--- src/test/java/guru/springframework/msscjacksonexamples/model/BaseTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BaseTest.java
@@ -1,10 +1,19 @@
 package guru.springframework.msscjacksonexamples.model;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@JsonTest
 public class BaseTest {
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
     protected BeerDto getDto() {
         return BeerDto.builder()
                 .beerName("BeerName")
--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("snake")
+@ActiveProfiles("kebab")
 @JsonTest
-public class BeerDtoSnakeTest extends BaseTest{
+public class BeerDtoKebabTest extends BaseTest{
 
     @Test
-    void testSnake() throws JsonProcessingException {
+    void testKebab() throws JsonProcessingException {
+
         BeerDto dto = getDto();
 
         String json = objectMapper.writeValueAsString(dto);
